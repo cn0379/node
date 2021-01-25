65d71a575375fb440782970c6bb64673e3394814
cn0379 <cn0379@outlook.com>
2021-01-26 00:21:16 +0800

    与数据库建立连接

--- app.js
+++ app.js
@@ -1,17 +1,69 @@
 /*
  * @Author: your name
  * @Date: 2021-01-19 23:18:45
- * @LastEditTime: 2021-01-19 23:26:07
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit()
- * @FilePath: \express+mongodb\app.js
+ * @LastEditTime: 2021-01-26 00:18:14
  */
-const express = require('express')
 
-const app = express()
+// 引入mongodb
+const mongoose = require('mongoose')
 
-app.get('/login', (req, res) => {
-  res.send('hellow')
+// 建立连接
+mongoose.connect('mongodb://127.0.0.1:27017/mydb')
+
+// 操作admin表 定义一个schema
+const AdminSchema = mongoose.Schema({
+  name: 'String',
+  age: Number,
+  status: Number
 })
 
-app.listen(3000)
+// 定义数据库模型
+const User = mongoose.model('User', AdminSchema)
+
+// 查询
+// User.find({}, (err, res) => {
+//   if (err) {
+//     console.log(err);
+//     return
+//   }
+//   console.log(res);
+// })
+
+// 增加
+// const data = new User({
+//   name: '张少隆',
+//   age: 22,
+//   status: 0
+// })
+
+// data.save((err, res) => {
+//   if (err) {
+//     return
+//   }
+//   console.log(res);
+// })
+
+// 更新
+
+const data = new User({
+  name: '张少隆',
+  age: 18,
+  status: 0
+})
+
+data.update({ "_id": '600eea71f06c492778a5c277' }, { "name": 5555 }, (err, res) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(res);
+})
+
+// const express = require('express')
+
+// const app = express()
+
+// app.get('/login', (req, res) => {
+//   res.send('hellow')
+// })
+
+// app.listen(3000)
