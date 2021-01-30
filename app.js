015a64cc9b579194275e4acca8ccd1054d3cb760
cn0379 <cn0379@outlook.com>
2021-01-30 23:54:05 +0800

    node 爬取图片

--- app.js
+++ app.js
@@ -3,12 +3,36 @@
  * @Date: 2021-01-19 23:18:45
  */
 
-var UserModel = require('./modules/user')
+// var UserModel = require('./modules/user')
 
-UserModel.find({}, (err, res) => {
-  if (err) {
-    console.log(err);
-    return
-  }
-  console.log(res);
+// UserModel.find({}, (err, res) => {
+//   if (err) {
+//     console.log(err);
+//     return
+//   }
+//   console.log(res);
+// })
+
+const http = require('http')
+const cheerio = require('cheerio')
+let HOST = 'http://web.itheima.com/'
+
+let req = http.request( HOST + 'teacher.html', res => {
+  let chunks = []
+
+  res.on('data', chunk => {
+    chunks.push(chunk)
+  })
+
+  res.on('end', () => {
+    let htmlstr = Buffer.concat(chunks).toString('utf-8')
+    let $ = cheerio.load(htmlstr)
+    // $('.tea_main .tea_box4 .main_pic > img').each( (idx , item) => {
+    //   console.log(HOST + $(item).attr('src'));
+    // } )
+    let imgs = Array.prototype.map.call($('.tea_main .tea_box4 .main_pic > img'),item => HOST + $(item).attr('src'))
+    console.log(imgs);
+  })
 })
+
+req.end()
